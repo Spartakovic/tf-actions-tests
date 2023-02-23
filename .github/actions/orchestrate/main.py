72ba29fe07c391d957c94fa74a2f8885d949c7e5
Dias Saparov <dias@digger.dev>
2023-02-23 15:39:17 +0000

    remove .idea

--- .github/actions/orchestrate/main.py
+++ .github/actions/orchestrate/main.py
@@ -8,6 +8,7 @@ from github import Github
 path = sys.argv[0]
 
 event = json.loads(os.getenv("GITHUB_EVENT"))
+event_name = os.getenv("GITHUB_EVENT_NAME")
 github_token = os.getenv("GITHUB_TOKEN")
 github_repo = os.getenv("GITHUB_REPOSITORY")
 
@@ -18,7 +19,7 @@ oldest_open_pr = all_open_prs[0] if all_open_prs.totalCount > 0 else None
 all_closed_prs = github.get_repo(github_repo).get_pulls(state='closed', sort='updated', direction='desc')
 latest_closed_pr = all_closed_prs[0] if all_closed_prs.totalCount > 0 else None
 
-if event['name'] == 'pull_request':
+if event_name == 'pull_request':
     if event['action'] in ['opened', 'reopened', 'synchronize']:
         if not all_open_prs:
             print("No open PRs found. Running terraform plan")
@@ -27,12 +28,12 @@ if event['name'] == 'pull_request':
     if event['action'] in ['closed']:
         if latest_closed_pr.number == event['number']:
             github.get_repo(github_repo).get_workflow("orchestration.yml").create_dispatch(oldest_open_pr.ref, {"command_type": "plan"})
-elif event['name'] == 'push':
+elif event_name == 'push':
     if oldest_open_pr.head.ref == event['ref']:
         print("This is the oldest open PR. Running terraform plan")
     else:
         print("This is not the oldest open PR. Skipping terraform plan")
-elif event['name'] == 'workflow_dispatch':
+elif event_name == 'workflow_dispatch':
     if event['inputs']['command_type'] == 'plan':
         print("Running terraform plan")
     elif event['inputs']['command_type'] == 'apply':
