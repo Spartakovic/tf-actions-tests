51bf7d47ded2e2386f8f1da468fdeefb13a0f47f
Dias Saparov <dias@digger.dev>
2023-02-23 16:22:33 +0000

    remove .idea

--- .github/actions/orchestrate/main.py
+++ .github/actions/orchestrate/main.py
@@ -29,14 +29,9 @@ if event_name == 'pull_request':
         if latest_closed_pr.number == event['number']:
             github.get_repo(github_repo).get_workflow("orchestration.yml").create_dispatch(oldest_open_pr.ref, {"command_type": "plan"})
 elif event_name == 'push':
-    print(oldest_open_pr.head.sha)
-    print(oldest_open_pr.head.ref)
-    print(oldest_open_pr.base.ref)
-    print(oldest_open_pr.base.sha)
-    print(event)
     if not oldest_open_pr:
         print("No pr's open. Running terraform plan")
-    elif oldest_open_pr.head.ref == event['ref']:
+    elif oldest_open_pr.head.sha == event['after']:
         print("This is the oldest open PR. Running terraform plan")
     else:
         print("This is not the oldest open PR. Skipping terraform plan")
